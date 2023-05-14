6c6610a4870426390a42354c30db4a7c83b428cc
Solomon_Muhye <132606059+SolomonMuhyeY@users.noreply.github.com>
2023-05-14 03:59:34 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,1 @@
-import Header from "./components/Header";
-import MemeCard from "./components/MemeCard";
-const App = function () {
-  return (
-    <div className='container'>
-      <Header />
-      <MemeCard />
-    </div>
-  );
-};
-export default App;
+
