f172847de8cb18e6cac60dab744f5ccd13a38d82
Solomon_Muhye <132606059+SolomonMuhyeY@users.noreply.github.com>
2023-05-14 03:50:17 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import MemeCard from "./components/MemeCard";
+const App = function () {
+  return (
+    <div className='container'>
+      <Header />
+      <MemeCard />
+    </div>
+  );
+};
+export default App;
